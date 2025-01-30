ee6dd9279784d6fabd8673293acae1c2af258258
nixywex <nixywex@gmail.com>
2025-01-30 14:44:09 +0100

    Initial Commit

---
+++ ai-chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ai-chat
+//
+//  Created by Nikita Sheludko on 30.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ai-chat/ai_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_chatApp.swift
+//  ai-chat
+//
+//  Created by Nikita Sheludko on 30.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ai_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
