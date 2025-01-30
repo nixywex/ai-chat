e8dec9fc3742a3c3b8b8faca12645f93cc334a74
nixywex <nixywex@gmail.com>
2025-01-30 16:04:30 +0100

    first commit

--- ai-chat/ContentView.swift
+++ ai-chat/ContentView.swift
@@ -6,16 +6,86 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
 
 struct ContentView: View {
+    @State private var text = ""
+    @State private var userMessage = ""
+    @State private var aiMessage = ""
+    let model = GenerativeModel(name: "gemini-1.5-flash-8b", apiKey: "")
+    
+    func sendMessage() {
+        let promt = text
+        self.userMessage = promt
+        self.text = ""
+        
+        Task {
+            do {
+                let responce = try await model.generateContent(promt)
+                aiMessage = responce.text ?? "No response"
+            }
+            catch {
+                aiMessage = "Error"
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                if (!userMessage.isEmpty) {
+                    HStack {
+                        Spacer()
+                        
+                        VStack {
+                            Text(userMessage)
+                                .font(.system(size: 18))
+                                .foregroundStyle(.white)
+                                .padding()
+                        }
+                        .frame(maxWidth: 250, alignment: .leading)
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                        .padding()
+                    }
+                }
+                
+                if (!aiMessage.isEmpty) {
+                    HStack {
+                        VStack {
+                            Text(aiMessage)
+                                .font(.system(size: 18))
+                                .foregroundStyle(.black)
+                                .padding()
+                            
+                        }
+                        .background(Color(red: 216 / 255, green: 216 / 255, blue: 216 / 255))
+                        .cornerRadius(15)
+                        .frame(maxWidth: 250)
+                        .padding()
+                        
+                        Spacer()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Enter your question", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.leading, 10)
+                    .font(.system(size: 20))
+                Button(action: {
+                    sendMessage()
+                }){
+                    Image(systemName: "paperplane.circle.fill")
+                        .font(.system(size: 30))
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.bottom, 10)
         }
-        .padding()
     }
 }
 
