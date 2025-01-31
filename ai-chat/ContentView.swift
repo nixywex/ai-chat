9833cced07a2c142899deb2b8e2e5d591932d6f5
nixywex <nixywex@gmail.com>
2025-01-31 18:55:11 +0100

    fixed messages display

--- ai-chat/ContentView.swift
+++ ai-chat/ContentView.swift
@@ -15,6 +15,12 @@ struct ContentView: View {
     let model = GenerativeModel(name: "gemini-1.5-flash-8b", apiKey: "")
     
     func sendMessage() {
+        if (!validate(text)) {
+            return
+        }
+        
+        hideKeyboard()
+        
         let promt = text
         self.userMessage = promt
         self.text = ""
@@ -31,61 +37,74 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            ScrollView {
-                if (!userMessage.isEmpty) {
-                    HStack {
-                        Spacer()
-                        
-                        VStack {
-                            Text(userMessage)
-                                .font(.system(size: 18))
-                                .foregroundStyle(.white)
-                                .padding()
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    if (!userMessage.isEmpty) {
+                        HStack {
+                            Spacer()
+                            
+                            VStack {
+                                Text(userMessage)
+                                    .font(.system(size: 18))
+                                    .foregroundStyle(.white)
+                                    .padding()
+                            }
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                            .frame(minWidth: 50, maxWidth: 250, alignment: .trailing)
+                            .padding(.horizontal)
                         }
-                        .frame(maxWidth: 250, alignment: .leading)
-                        .background(Color.blue)
-                        .cornerRadius(15)
-                        .padding()
+                    }
+                    
+                    if (!aiMessage.isEmpty) {
+                        HStack {
+                            VStack {
+                                Text(aiMessage)
+                                    .font(.system(size: 18))
+                                    .foregroundStyle(.black)
+                                    .padding()
+                                
+                            }
+                            .background(Color(red: 216 / 255, green: 216 / 255, blue: 216 / 255))
+                            .cornerRadius(15)
+                            .frame(minWidth: 50, maxWidth: 250, alignment: .leading)
+                            .padding(.horizontal)
+                            
+                            Spacer()
+                        }
                     }
                 }
                 
-                if (!aiMessage.isEmpty) {
-                    HStack {
-                        VStack {
-                            Text(aiMessage)
-                                .font(.system(size: 18))
-                                .foregroundStyle(.black)
-                                .padding()
-                            
-                        }
-                        .background(Color(red: 216 / 255, green: 216 / 255, blue: 216 / 255))
-                        .cornerRadius(15)
-                        .frame(maxWidth: 250)
-                        .padding()
-                        
-                        Spacer()
+                Spacer()
+                
+                HStack {
+                    TextField("Enter your question", text: $text)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading, 10)
+                        .font(.system(size: 20))
+                    Button(action: {
+                        sendMessage()
+                    }){
+                        Image(systemName: "paperplane.circle.fill")
+                            .font(.system(size: 30))
                     }
                 }
+                .padding(.horizontal, 10)
+                .padding(.bottom, 10)
             }
-            
-            Spacer()
-            
-            HStack {
-                TextField("Enter your question", text: $text)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.leading, 10)
-                    .font(.system(size: 20))
-                Button(action: {
-                    sendMessage()
-                }){
-                    Image(systemName: "paperplane.circle.fill")
-                        .font(.system(size: 30))
-                }
-            }
-            .padding(.horizontal, 10)
-            .padding(.bottom, 10)
+            .navigationTitle("AI Chat")
         }
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    func validate(_ text: String) -> Bool {
+        return !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
 }
 
